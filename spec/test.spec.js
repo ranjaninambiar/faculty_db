cc2069876ca8a7ea2677747462f40661649c852b
Ganesh-U <u.ganesh100@gmail.com>
2021-06-11 14:33:14 +0530

    unit test complete

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -14,20 +14,185 @@ describe('Faculty Dashboard', () => {
         })
     });
 
-    it('File Upload Page', (done) => {
+    it('Should Render User Login Page', (done) => {
         let options = {
-            url: `${test_config.baseURL}/upload/609d693af4bc0842483779f7/60689db3d0e80797a4b68d67`   
+            url: `${test_config.baseURL}/auth/user-login`   
         };
         Request.get(options,(err,res)=>{
-            expect(res.statusCode).toBe(200)
-            expect(res.body).toContain('html')
-            done();
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
         })
     });
 
-    it('File Upload Page 2', (done) => {
+    it('Should Render a popup Page', (done) => {
         let options = {
-            url: `${test_config.baseURL}/upload/abcd/abcd`   
+            url: `${test_config.baseURL}/auth/popup`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render Forgot Password Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/auth/user-fpass`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render reset Link Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/auth/user-resetlink`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render ToDo Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/auth/user-todo`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render Calender Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/auth/user-cal`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render Forgot Password Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/auth/user-fpassw`   
+        };
+        Request.post(options,(err,res)=>{
+                expect(res.statusCode).toBe(404)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+    
+    it('Should Render Logout Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/auth/user-logout`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render SignUp Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/auth/user-signUp`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render Google Sign-Up Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/auth/user-gsignUp`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render User Front Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/user/1`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render User Profile Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/user/1/profile`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render ToDo Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/user/todo`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render Calender Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/user/calender`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render Courses Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/user/1/courses`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render NewNote Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/user/1/notes-newnote`   
+        };
+        Request.get(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('File Upload Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/upload/609d693af4bc0842483779f7/60689db3d0e80797a4b68d67`   
         };
         Request.get(options,(err,res)=>{
             expect(res.statusCode).toBe(200)
@@ -56,16 +221,6 @@ describe('Faculty Dashboard', () => {
         })
     });
 
-    it('Files Page 3', (done) => {
-        let options = {
-            url: `${test_config.baseURL}${test_config.test4}`   
-        };
-        Request.get(options,(err,res)=>{
-            expect(res.statusCode).toBe(200)
-            done();
-        })
-    });
-
     it('Files Page 4', (done) => {
         let options = {
             url: `${test_config.baseURL}/files/bbf5e7ea0e95db128f24db4323e69d73`   
@@ -86,17 +241,6 @@ describe('Faculty Dashboard', () => {
         })
     });
 
-    it('Image Page 2', (done) => {
-        let options = {
-            url: `${test_config.baseURL}${test_config.test5}`   
-        };
-        Request.get(options,(err,res)=>{
-            expect(res.statusCode).toBe(404)
-            done();
-        })
-    });
-   
-
     it('Assign work', (done) => {
         let options = {
             url: `${test_config.baseURL}/upload/609d693af4bc0842483779f7/60689db3d0e80797a4b68d67`,
@@ -107,13 +251,36 @@ describe('Faculty Dashboard', () => {
         })
     });
 
-    it('Assign work 2', (done) => {
+    it('Should Render User Login Page Post', (done) => {
         let options = {
-            url: `${test_config.baseURL}${test_config.test1}`,
+            url: `${test_config.baseURL}/auth/user-login`   
         };
         Request.post(options,(err,res)=>{
-            expect(res.statusCode).toBe(302);
-            done();
+                expect(res.statusCode).toBe(302)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render SignUp Page-post', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/auth/user-signUp`   
+        };
+        Request.post(options,(err,res)=>{
+                expect(res.statusCode).toBe(200)
+                expect(res.headers).toBeDefined()
+                done();
+        })
+    });
+
+    it('Should Render NewNote Page', (done) => {
+        let options = {
+            url: `${test_config.baseURL}/auth/user-fpass`   
+        };
+        Request.post(options,(err,res)=>{
+                expect(res.statusCode).toBe(302)
+                expect(res.headers).toBeDefined()
+                done();
         })
     });
 
@@ -128,49 +295,9 @@ describe('Faculty Dashboard', () => {
         })
     });
 
-    it('Issue Course 2', (done) => {
-        let options = {
-            url: `${test_config.baseURL}/courses/d48a7065/issue/a4b68d67`,
-        };
-        Request.post(options,(err,res)=>{
-            expect(res.statusCode).toBe(302);
-            done();
-        })
-    });
-
-    it('Issue Course 3', (done) => {
-        let options = {
-            url: `${test_config.baseURL}${test_config.test2}`,
-        };
-        Request.post(options,(err,res)=>{
-            expect(res.statusCode).toBe(302);
-            done();
-        })
-    });
-
-    it('Issue Course 4', (done) => {
-        let options = {
-            url: `${test_config.baseURL}/courses/abcd/issue/abcd`,
-        };
-        Request.post(options,(err,res)=>{
-            expect(res.statusCode).toBe(302);
-            done();
-        })
-    });
-
     it('Delete File', (done) => {
         let options = {
             url: `${test_config.baseURL}/files/609e7b13de1ae92f0c2d6b3a`,
-        };
-        Request.delete(options,(err,res)=>{
-            expect(res.statusCode).toBe(302);
-            done();
-        })
-    });
-
-    it('Delete File 2', (done) => {
-        let options = {
-            url: `${test_config.baseURL}${test_config.test3}`,
         };
         Request.delete(options,(err,res)=>{
             expect(res.statusCode).toBe(302);
